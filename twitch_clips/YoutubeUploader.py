b7f064e09b5ec2b15578031dad77458694411f10
Ninzalo <k-artemiy@mail.ru>
2024-05-03 20:20:39 +0300

    refactor(youtube-uploader): minor refactor

--- twitch_clips/YoutubeUploader.py
+++ twitch_clips/YoutubeUploader.py
@@ -6,13 +6,15 @@ from .Logger import BaseLogger, Logger
 class YoutubeUploader:
     def __init__(self, cookies_path: str, logger: BaseLogger | None = None) -> None:
         self.logger = logger if logger else Logger()
+        self.cookies_path = cookies_path
         self.uploader = YTUploaderSession.from_cookies_txt(
             cookies_txt_path=cookies_path
         )
-        self.logger.log("Validating cookies...")
-        self._validate_cookies()
-        self.logger.log("Cookies validated!")
 
-    def _validate_cookies(self) -> None:
+    def has_valid_cookies(self) -> bool:
+        self.logger.log("Validating cookies...")
         if not self.uploader.has_valid_cookies():
-            raise Exception("Invalid cookies provided")
+            self.logger.log("Invalid cookies provided, or cookies file not found")
+            return False
+        self.logger.log("Cookies validated!")
+        return True
